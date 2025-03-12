d0f199a23e91c9458651a037ff0c5fa5ff2120df
francoaguilar622 <123565045+francoaguilar622@users.noreply.github.com>
2025-03-12 00:44:53 +0000

    trabajoterminado

--- .learn/resets/11-homework/app.js
+++ .learn/resets/11-homework/app.js
@@ -2,34 +2,35 @@ const sum = function(a, b) {
     return a + b;
 };
 
-
-let oneEuroIs = {
-    "JPY": 156.5, // japan yen
-    "USD": 1.07, // us dollar
-    "GBP": 0.87, // british pound
-}
-
 // esta es mi función que suma dos números
     // declaramos una funcion con el mismo nombre "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro){
     // convertimos el valor a dolares
-    let valueInDollar = valueInEuro * oneEuroIs["USD"];
+    let valueInDollar = valueInEuro * 1.2;
     // retornamos el valor
-    return Number(valueInDollar.toFixed(2));
+    return valueInDollar;
 }
 const fromDollarToYen = function(valueInDollar) {
     // convertimos el valor a yenes
-    let valueInYen = (valueInDollar / oneEuroIs["USD"]) * oneEuroIs["JPY"];
+    let valueInYen = valueInDollar * 4.2;
     // retornamos el valor
-    return Number(valueInYen.toFixed(2));
+    return valueInYen;
 };
 
 const fromYenToPound = function(valueInYen) {
     // convertimos el valor a libras
-    let valueInPound = (valueInYen / oneEuroIs["JPY"]) * oneEuroIs["GBP"];
+    let valueInPound = valueInYen * 4.2;
     // retornamos el valor
-    return Number(valueInPound.toFixed(2));
+    return valueInPound;
 };
+
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
 
 // solo un registro en consola para nosotros.
 
